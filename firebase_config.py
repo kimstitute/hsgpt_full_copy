6800bb68bbbb139b62d7ca410bc034c2fc2eb5a1
kimstitute <152252175+kimstitute@users.noreply.github.com>
2024-11-15 13:10:10 +0900

    Add files via upload

--- firebase_config.py
+++ firebase_config.py
@@ -2,11 +2,12 @@ import firebase_admin
 from firebase_admin import credentials, firestore, storage
 
 # Firebase 서비스 계정 키 경로
-cred = credentials.Certificate("gpt-chating-c72cacf446d4.json")
+# cred = credentials.Certificate("gpt-chating-c72cacf446d4.json")
+cred = credentials.Certificate("hs-gpt-b4da9-firebase-adminsdk-jhqn3-13e1e5b847.json")
 
 # Firebase 초기화 (Storage 버킷 이름 포함)
 firebase_admin.initialize_app(cred, {
-    'storageBucket': 'gpt-chating.appspot.com'  # Firebase Storage 버킷 이름 설정
+    'storageBucket': 'hs-gpt-b4da9.appspot.com'  # Firebase Storage 버킷 이름 설정
 })
 
 # Firestore 클라이언트 초기화
